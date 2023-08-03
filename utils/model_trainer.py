e2977c583c2ada4dadeda5a216d2bd12088a1d80
Ayikanying-ux <allanayikanying72@gmail.com>
2023-08-04 02:29:10 +0300

    made changes

--- utils/model_trainer.py
+++ utils/model_trainer.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
 
 class ModelTrainer:
     def __init__(self, data_loader, feature_extractor):
@@ -14,8 +14,8 @@ class ModelTrainer:
         self.feature_extractor = feature_extractor
 
     def merge_dataframe(self):
-        df_snoring = pd.read_csv("../dataset/csv/snoring_data.csv")
-        df_non_snoring = pd.read_csv("../dataset/csv/non_snoring_data.csv")
+        df_snoring = pd.read_csv("dataset/csv/snoring_data.csv")
+        df_non_snoring = pd.read_csv("dataset/csv/non_snoring_data.csv")
         df = pd.concat([df_snoring, df_non_snoring])
 
         df['Features'] = df['Features'].apply(lambda x: np.array(x.strip('[]').split()).astype(float))
@@ -24,8 +24,9 @@ class ModelTrainer:
         df.drop(columns=['audio', 'Labels', 'Audio_Data', 'Sample_Rate'], inplace=True)
         return df
 
-    def model_trainer(self):
+    def train_model(self):
         df = self.merge_dataframe()
+        print(df)
         X = df['Features'].to_list()
         y = df['class_id']
 
@@ -48,6 +49,8 @@ class ModelTrainer:
         classifier.fit(X_train, y_train)
 
         y_pred = classifier.predict(X_test)
+
+        report = classification_report(y_test, y_pred)
         
         cm = confusion_matrix(y_test, y_pred)
         # Create a heatmap of the confusion matrix
@@ -56,5 +59,8 @@ class ModelTrainer:
         plt.ylabel("True Labels")
         plt.title("Confusion Matrix")
         plt.show()
-        return y_pred, cm
+        #print(y_pred)
+
+        print("Classification Report:")
+        print(report)
         
