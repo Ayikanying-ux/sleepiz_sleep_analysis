3f91586f2d9df2ea07740205d087560f40427174
Ayikanying-ux <allanayikanying72@gmail.com>
2023-08-26 22:27:37 +0300

    restructured my code

--- src/IO/dataLoader.py
+++ src/IO/dataLoader.py
@@ -4,17 +4,18 @@ import pandas as pd
 class DataLoader:
     def __init__(self, folder_path):
         self.folder_path = Path(folder_path)
-
-    def get_data_as_dataframe(self):
+        self._csv_path = "data/"
+    def load_files_to_dataframe(self):
         data = []
 
         for file in self.folder_path.iterdir():
             if file.is_file():
                 label = int(file.parent.name[-1])  # Extract label from parent folder name
                 class_name = 'snoring' if label == 1 else 'non_snoring'
-                data.append({'filename': file.name, 'id': label, 'class': class_name})
+                data.append({'filename': file.name, 'class': label, 'label': class_name})
 
         df = pd.DataFrame(data)
+        df.to_csv(self._csv_path)
         return df
 
     
