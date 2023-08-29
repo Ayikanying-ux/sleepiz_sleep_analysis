252c2869a9ef2d62064dd890e4d4cda525b30fda
Ayikanying-ux <allanayikanying72@gmail.com>
2023-08-29 16:29:02 +0300

    added a file for reading audio files

---
+++ src/IO/readAudioFiles.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+class ReadAudioFiles():
+    def __init__(self, audio_path) -> None:
+        self.audio_path = Path(audio_path)
+
+    def read_audio_files_in_folder(self, subfolder='.', file_ending='wav'):
+        audio_file_names = (self.audio_path / subfolder).glob('*.' + file_ending)
+        audio_data = []
+        for file_name in audio_file_names:
+            audio_data.append(file_name)
+        return audio_data
