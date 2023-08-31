955704b12ef76ed7accfdd8d01a5df13f070c5bb
Ayikanying-ux <allanayikanying72@gmail.com>
2023-08-31 14:09:11 +0300

    restructure

--- src/FeatureExtraction/extract_features.py
+++ src/FeatureExtraction/extract_features.py
@@ -1,17 +1,15 @@
 import numpy as np
 import librosa
 
-class ExtractMFCCFeatures:
-    def __init__(self, num_mfcc=20, num_filter_banks=26):
-        self.num_mfcc = num_mfcc
-        self.num_filter_banks = num_filter_banks
 
-    def extract_mfcc_features(self, audio_data : np.array, sample_rate):
-        filter_banks = librosa.filters.mel(sr=sample_rate, n_fft=2048, n_mels=self.num_filter_banks)
-        filter_banks_features = np.dot(filter_banks, np.abs(librosa.stft(audio_data))**2.0)
+def extract_mfcc_features(audio_data: np.array, 
+                          sample_rate: int, num_mfcc: int = 20, 
+                          num_filter_banks: int = 26) -> np.array:
+    filter_banks = librosa.filters.mel(sr=sample_rate, n_fft=2048, n_mels=num_filter_banks)
+    filter_banks_features = np.dot(filter_banks, np.abs(librosa.stft(audio_data)) ** 2.0)
 
-        mfcc = librosa.feature.mfcc(S=librosa.power_to_db(filter_banks_features), n_mfcc=self.num_mfcc)
-        mfcc_mean = np.mean(mfcc, axis=1)
-        mfcc_std = np.std(mfcc, axis=1)
-        mfcc_features = np.concatenate((mfcc_mean, mfcc_std))
-        return mfcc_features
+    mfcc = librosa.feature.mfcc(S=librosa.power_to_db(filter_banks_features), n_mfcc=num_mfcc)
+    mfcc_mean = np.mean(mfcc, axis=1)
+    mfcc_std = np.std(mfcc, axis=1)
+    mfcc_features = np.concatenate((mfcc_mean, mfcc_std))
+    return mfcc_features
--- src/IO/dataLoader.py
+++ src/IO/dataLoader.py
@@ -1,22 +1,38 @@
 from pathlib import Path
+import numpy as np
 import pandas as pd
+import librosa
+from ..FeatureExtraction.extract_features import extract_mfcc_features
 
-class DataLoader:
-    def __init__(self, folder_path, filename: str):
-        self.folder_path = Path(folder_path)
-        self.filename = filename
-        self._csv_path = "../data/"
-    def load_files_to_dataframe(self):
+class AudioLoader:
+    def __init__(self):
+        self._csv_path = Path("../data/csv/")
+        self._extract_feature = extract_mfcc_features
+        
+    def load_files_to_dataframe(self, folder_path:str):
         data = []
 
-        for file in self.folder_path.iterdir():
+        for file in folder_path.iterdir():
             if file.is_file():
+                audio_file, sample_rate = librosa.load(file)
                 label = int(file.parent.name[-1])  # Extract label from parent folder name
                 class_name = 'snoring' if label == 1 else 'non_snoring'
-                data.append({'filename': file.name, 'class': label, 'label': class_name})
+                data.append({'Audio': audio_file, 'label': class_name, "sample_rate": sample_rate})
 
         df = pd.DataFrame(data)
-        df.to_csv(self._csv_path + self.filename + ".csv")
+
+        #df.to_csv(self._csv_path / f"{data[0]['label']}.csv", index=False)
         return df
+
+    
+    def merge_dataframes(self, data_filepath1: str, data_filepath2: str):
+        data_file1 = pd.read_csv(data_filepath1)
+        data_file2 = pd.read_csv(data_filepath2)
+        dataframe = pd.concat([data_file1, data_file2])
+        
+        output_filepath = self._csv_path / "data.csv"
+        dataframe.to_csv(output_filepath, index=False)
+        
+        return dataframe
 
     
