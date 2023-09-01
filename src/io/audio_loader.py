c04f9b3223a0fd47294861a89e48157820990921
Jan Mikelson <jan.mikelson@gmail.com>
2023-09-01 14:38:34 +0200

    I modified the audio loader to take the label as an argument

--- src/io/audio_loader.py
+++ src/io/audio_loader.py
@@ -2,27 +2,22 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import librosa
-from ..FeatureExtraction.extract_features import extract_mfcc_features
 
 class AudioLoader:
     def __init__(self):
         self._csv_path = Path("../data/csv/")
-        self._extract_feature = extract_mfcc_features
         
-    def load_files_to_dataframe(self, folder_path:str):
+    def load_files_from_folder(self, folder_path:str, label = "unlabled") -> pd.DataFrame:
         data = []
 
         for file in folder_path.iterdir():
             if file.is_file():
                 audio_file, sample_rate = librosa.load(file)
-                label = int(file.parent.name[-1])  # Extract label from parent folder name
-                class_name = 'snoring' if label == 1 else 'non_snoring'
-                data.append({'Audio': audio_file, 'label': class_name, "sample_rate": sample_rate})
+                data.append({'Audio': audio_file, 'label': label, "sample_rate": sample_rate})
 
         df = pd.DataFrame(data)
-
-        #df.to_csv(self._csv_path / f"{data[0]['label']}.csv", index=False)
         return df
+
 
     
     def merge_dataframes(self, data_filepath1: str, data_filepath2: str):
